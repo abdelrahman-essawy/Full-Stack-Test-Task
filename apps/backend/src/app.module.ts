976bdaf40505c4d60700bb9a7cbba532e1f8ec9e
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2025-03-21 15:42:56 +0200

    add TODO
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -14,6 +14,7 @@ import {
   ValidationExceptionFilter,
 } from './common/filters';
 
+// TODO: add 'nestjs-pino'
 @Module({
   imports: [ConfigAppModule, DatabaseModule, AuthModule, UserModule],
   controllers: [],
