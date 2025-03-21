7e8de8e189567fd81d9fa07ad4ffe8e82f188574
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2025-03-21 15:40:08 +0200

    feat(backend): apply filters
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,12 +1,41 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { ConfigAppModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
+import { ValidationError } from 'class-validator';
+import {
+  AllExceptionsFilter,
+  BadRequestExceptionFilter,
+  ForbiddenExceptionFilter,
+  NotFoundExceptionFilter,
+  UnauthorizedExceptionFilter,
+  ValidationExceptionFilter,
+} from './common/filters';
 
 @Module({
   imports: [ConfigAppModule, DatabaseModule, AuthModule, UserModule],
   controllers: [],
-  providers: [],
+  providers: [
+    { provide: APP_FILTER, useClass: AllExceptionsFilter },
+    { provide: APP_FILTER, useClass: ValidationExceptionFilter },
+    { provide: APP_FILTER, useClass: BadRequestExceptionFilter },
+    { provide: APP_FILTER, useClass: UnauthorizedExceptionFilter },
+    { provide: APP_FILTER, useClass: ForbiddenExceptionFilter },
+    { provide: APP_FILTER, useClass: NotFoundExceptionFilter },
+    {
+      // Allowing to do validation through DTO
+      // Since class-validator library default throw BadRequestException, here we use exceptionFactory to throw
+      // their internal exception so that filter can recognize it
+      provide: APP_PIPE,
+      useFactory: () =>
+        new ValidationPipe({
+          exceptionFactory: (errors: ValidationError[]) => {
+            return errors[0];
+          },
+        }),
+    },
+  ],
 })
 export class AppModule {}
