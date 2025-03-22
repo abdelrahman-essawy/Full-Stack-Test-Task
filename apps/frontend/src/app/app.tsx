d4abfefe1b86e1d207593b03ef698c90f3657f3b
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2025-03-22 21:55:15 +0200

    feat(frontend): basic form ui
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,39 +1,12 @@
-import NxWelcome from './nx-welcome';
 import '../styles.css';
-
-import { Route, Routes, Link } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
+import { SignupForm } from '../modules/auth/components/form';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="@easygenerator/frontend" />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
+        <Route path="/" element={<SignupForm />} />
         <Route
           path="/page-2"
           element={
@@ -43,7 +16,6 @@ export function App() {
           }
         />
       </Routes>
-      {/* END: routes */}
     </div>
   );
 }
