1055e9617ed16a6eb69ab9438fa3b499d1650e90
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2025-03-23 12:28:58 +0200

    feat(backend): use validation lib
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- apps/backend/src/modules/auth/dtos/signup.req.dto.ts
+++ apps/backend/src/modules/auth/dtos/signup.req.dto.ts
@@ -1,18 +1,4 @@
 import { createZodDto } from 'nestjs-zod';
-import { z } from 'zod';
-
-export const signUpSchema = z.object({
-  email: z.string().email('Invalid email format'),
-  name: z.string().min(3, 'Name must be at least 3 characters'),
-  password: z
-    .string()
-    .min(8, 'Password must be at least 8 characters long')
-    .regex(/[A-Za-z]/, 'Password must contain at least one letter')
-    .regex(/[0-9]/, 'Password must contain at least one number')
-    .regex(
-      /[^A-Za-z0-9]/,
-      'Password must contain at least one special character'
-    ),
-});
+import { signUpSchema } from '@easygenerator/validations';
 
 export class SignupReqDto extends createZodDto(signUpSchema) {}
