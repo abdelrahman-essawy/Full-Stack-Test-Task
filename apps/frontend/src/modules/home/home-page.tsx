f615dc4a5b41325af1e905fb15a92800accc0f34
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2025-03-24 08:46:15 +0200

    refactor(frontend): improve ui in home page
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- apps/frontend/src/modules/home/home-page.tsx
+++ apps/frontend/src/modules/home/home-page.tsx
@@ -1,17 +1,19 @@
 ﻿import { Cover } from '../../ui/cover';
 import { useAuthStore } from '../auth/use-auth-store';
+import { Container } from '../../components/container';
 
 export const HomePage = () => {
   const name = useAuthStore((state) => state.user?.name);
   return (
-    <div>
+    <Container>
       <h2 className="bg-clip-text  text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
-        Welcome to the <Cover>Application!</Cover>
+        Welcome to the{' '}
+        <Cover className={'bg-clip-text text-primary/80'}>Application!</Cover>
       </h2>
-      <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
+      <p className="max-w-2xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
         Huge thanks to <span className="text-primary font-bold">{name}</span>{' '}
         for testing my website! You made history… kind of. 😆
       </p>
-    </div>
+    </Container>
   );
 };
